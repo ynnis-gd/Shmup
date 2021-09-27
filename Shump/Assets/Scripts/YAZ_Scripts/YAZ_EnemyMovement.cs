2924e752aad31adf65c7366cdb03980c3194abf1
ynnis-gd <80097877+ynnis-gd@users.noreply.github.com>
2021-09-27 12:55:24 +0200

    Dév du mouvement ennemi
    - Ajout de 4 variables permettant à l'ennemi de se repositionner s'il sort de l'écran
    - Ajout d'une variable qui permet à l'ennemi de ne pas bouger s'il est à une hauteur proche du joueur

--- Shump/Assets/Scripts/YAZ_Scripts/YAZ_EnemyMovement.cs
+++ Shump/Assets/Scripts/YAZ_Scripts/YAZ_EnemyMovement.cs
@@ -8,6 +8,16 @@ public class YAZ_EnemyMovement : MonoBehaviour
     private Rigidbody2D enemyRb;
     private Transform playerTf;
     public float speed = 2;
+    
+    //Variable pour bouger le vaisseau que si le joueur est trop loin verticalement
+    public float maxProximity = 0.05f;
+
+    //Variables pour rediriger l'ennemi s'il va en dehors de l'écran de jeu
+    public float upperLimit = 7;
+    public float upperReposition = 4;
+    public float lowerLimit = -7;
+    public float lowerReposition = -4;
+    private bool hasReachedLimit = false;
 
     void Awake()
     {
@@ -16,14 +26,70 @@ public class YAZ_EnemyMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        //L'ennemi est au dessus du joueur
-        if (transform.position.y >= YAZ_GameManager.instance.playerShip.transform.position.y)
+        if (!hasReachedLimit)
         {
-            enemyRb.MovePosition((Vector2)transform.position + Vector2.up * speed * Time.fixedDeltaTime);
+            MoveEnemy();
         }
         else
         {
-            enemyRb.MovePosition((Vector2)transform.position + Vector2.down * speed * Time.fixedDeltaTime);
+            Recenter();
         }
     }
+
+    
+
+    private void MoveEnemy()
+    {
+        //L'ennemi est entre les deux limites
+        if (transform.position.y > YAZ_GameManager.instance.playerShip.transform.position.y + maxProximity)
+        {
+            Debug.Log("Je me déplace (vers bas)");
+            //L'ennemi est au dessus du joueur : il descend
+            enemyRb.MovePosition((Vector2) transform.position + Vector2.down * speed * Time.fixedDeltaTime);
+        }
+        else if (transform.position.y < YAZ_GameManager.instance.playerShip.transform.position.y - maxProximity)
+        {
+            Debug.Log("Je me déplace (vers haut)");
+            //L'ennemi est en dessous du joueur : il remonte
+            enemyRb.MovePosition((Vector2) transform.position + Vector2.up * speed * Time.fixedDeltaTime);
+        }
+        else
+        {
+            Debug.Log("coucou j'attaque");
+        }
+        
+        if (transform.position.y > upperLimit)
+        {
+            Debug.Log("trop haut !");
+            //L'ennemi est trop haut : on le redescend
+            hasReachedLimit = true;
+        }
+        else if (transform.position.y < lowerLimit)
+        {
+            Debug.Log("trop bas !");
+            //L'ennemi est trop bas : on le remonte
+            hasReachedLimit = true;
+        }
+
+
+        
+    }
+    private void Recenter()
+         {
+             if (transform.position.y >= upperReposition)
+             {
+                 enemyRb.MovePosition((Vector2) transform.position + Vector2.down * speed * Time.fixedDeltaTime);
+                 Debug.Log("Redescends !");
+             }
+             else if (transform.position.y <= lowerReposition)
+             {
+                 enemyRb.MovePosition((Vector2) transform.position + Vector2.up * speed * Time.fixedDeltaTime);
+                 Debug.Log("Remonte !");
+             }
+             else
+             {
+                 Debug.Log("C'est bon !");
+                 hasReachedLimit = false;
+             }
+         }
 }
